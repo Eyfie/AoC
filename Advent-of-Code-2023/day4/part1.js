88493c5cf65094d29412f671cfc95bff94856f05
Thomas Mendez de Vigo <Eyfie@mbp-de-thomas.home>
2023-12-04 13:24:55 +0100

    Feat: Added part 1 solution

---
+++ Advent-of-Code-2023/day4/part1.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const startTime = performance.now()
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+
+    let pilePoints = [];
+
+    const cards = data.split('\n').map((card) => {
+        const trimmedCard = card.split(': ').slice(-1);
+        const numberSets = trimmedCard[0].split(' | ');
+        const numberArrays = numberSets.map(numberStr => numberStr.split(' ').filter(number => number !== ''))
+        return numberArrays
+    })
+
+
+    cards.forEach((cardSets) => {
+
+        let cardPoints = 0.5;
+        let winningSet = false
+
+        const playedNumbers = cardSets[0];
+        const winningNumbers = cardSets[1];
+
+        for (let p = 0; p < playedNumbers.length; p++) {
+            for(let w = 0; w < winningNumbers.length; w++){
+    
+                if (playedNumbers[p] === winningNumbers[w]) {
+                    winningSet = true;
+                    cardPoints = cardPoints*2
+                }
+            }
+    
+        }
+
+        if(winningSet) {
+            pilePoints.push(cardPoints);
+            cardPoints = 0.5
+            winningSet = false
+        }
+    })
+    
+    console.log(pilePoints.reduce((a,b) => a+b));
+})
+
+
+
+const endTime = performance.now()
+console.log(`Time taken : ${parseFloat(endTime - startTime, 2).toFixed(2)}ms`)
